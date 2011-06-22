0c018b9badc8f3777f85efd46bf2f7638639bff4
pfeiffer@kde.org <pfeiffer@kde.org@7184536a-ea30-0410-a7dc-93bad6e629e8>
2011-06-22 09:41:15 +0000

    replace autogenerated copyright header with daisydiff one
    Also ignore hidden testdata directories, Patch by Don Willis <don.jp.willis@gmail.com>

--- daisydiff/src/test/org/outerj/daisy/diff/html/FileBasedTest.java
+++ daisydiff/src/test/org/outerj/daisy/diff/html/FileBasedTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Carsten Pfeiffer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.outerj.daisy.diff.html;
 
 import java.io.*;
@@ -140,10 +155,10 @@ public class FileBasedTest extends TestCase {
 		File[] tempTestDataDirs = aRootDir.listFiles(new FileFilter() {
 			
 			public boolean accept(File aFile) {
-				if (!aFile.isDirectory()) {
+				if (!aFile.isDirectory() || aFile.getName().startsWith(".")) {
 					return false;
 				}
-				
+
 				if (TestHelper.isTestDataDir(aFile)) {
 					if (ONLY_TESTS.isEmpty()) {
 						return true;
